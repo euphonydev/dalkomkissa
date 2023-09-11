7586d8e63bc15ff92328d978d5860bcf59e68a23
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-09-11 19:43:45 +0700

    Supabase setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,21 @@
 import createMiddleware from 'next-intl/middleware';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import type { Database } from '@/lib/database.types';
 
-export default createMiddleware({
-  locales: ['en', 'id'],
-  defaultLocale: 'en',
-});
+export default async function middleware(req: NextRequest) {
+  const handleI18nRouting = createMiddleware({
+    locales: ['en', 'id'],
+    defaultLocale: 'en',
+  });
+  const res = handleI18nRouting(req);
+  const supabase = createMiddlewareClient({ req, res });
+  await supabase.auth.getSession();
+
+  return res;
+}
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!api|auth|_next|.*\\..*).*)'],
 };
