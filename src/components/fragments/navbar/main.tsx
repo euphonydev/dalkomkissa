733809c4d0f0e8f08f80f6d33cbbdae7a16ef59f
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-09-12 13:23:23 +0700

    Show User data on nav

--- src/components/fragments/navbar/main.tsx
+++ src/components/fragments/navbar/main.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import * as React from "react"
+import React, { useState, useEffect } from "react";
 import Image from 'next/image'
 import Link from 'next/link'
-import { AlignLeftIcon, MoonIcon, SunIcon } from "lucide-react"
+import { AlignLeftIcon, MoonIcon, SunIcon, User } from "lucide-react"
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuGroup, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/components/ui/dropdown-menu'
@@ -14,6 +14,7 @@ import { useTranslations } from 'next-intl'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from "next/navigation"
+import '@/lib/utils/string/get-initial-name'
 
 const MainNavbar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => {
     const { theme, setTheme } = useTheme()
@@ -21,6 +22,26 @@ const MainNavbar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
     const t = useTranslations()
     const supabase = createClientComponentClient()
     const router = useRouter()
+    const [user, setUser] = useState<any>()
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        const getUser = async () => {
+            const { data: { user } } = await supabase.auth.getUser()
+            if (user) {
+                setIsLoggedIn(true)
+                const { data, error } = await supabase
+                    .from('profile')
+                    .select(`name, account (username)`)
+                    .eq('id', user.id)
+                    .single()
+                if (!error) {
+                    setUser(data)
+                }
+            }
+        }
+        getUser()
+    }, [])
 
     const handleLogout = async () => {
         const { error } = await supabase.auth.signOut()
@@ -62,38 +83,46 @@ const MainNavbar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
                             </DropdownMenuRadioGroup>
                         </DropdownMenuContent>
                     </DropdownMenu>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-                                <Avatar className="w-8 h-8">
-                                    <AvatarImage src="/avatars/01.png" alt="@imacool" />
-                                    <AvatarFallback>EU</AvatarFallback>
-                                </Avatar>
+                    {isLoggedIn ? (
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+                                    <Avatar className="w-8 h-8">
+                                        <AvatarImage src="/avatars/01.png" alt={'@' + (user?.account.username)} />
+                                        <AvatarFallback>{user?.name.getInitialName()}</AvatarFallback>
+                                    </Avatar>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56" align="end" forceMount>
+                                <DropdownMenuLabel className="font-normal">
+                                    <div className="flex flex-col space-y-1">
+                                        <p className="text-sm font-medium leading-none">{user?.name}</p>
+                                        <p className="text-xs leading-none text-muted-foreground">
+                                            {'@' + (user?.account.username)}
+                                        </p>
+                                    </div>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <Link href="/settings">
+                                        <DropdownMenuItem>
+                                            {t('SETTINGS')}
+                                        </DropdownMenuItem>
+                                    </Link>
+                                </DropdownMenuGroup>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <button onClick={handleLogout}>{t('LOGOUT')}</button>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    ) : (
+                        <Link href="/login">
+                            <Button variant="outline">
+                                {t('LOGIN')}
                             </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-56" align="end" forceMount>
-                            <DropdownMenuLabel className="font-normal">
-                                <div className="flex flex-col space-y-1">
-                                    <p className="text-sm font-medium leading-none">imacool</p>
-                                    <p className="text-xs leading-none text-muted-foreground">
-                                        example@mail.com
-                                    </p>
-                                </div>
-                            </DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuGroup>
-                                <Link href="/settings">
-                                    <DropdownMenuItem>
-                                        {t('SETTINGS')}
-                                    </DropdownMenuItem>
-                                </Link>
-                            </DropdownMenuGroup>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>
-                                <button onClick={handleLogout}>{t('LOGOUT')}</button>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>
