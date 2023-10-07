e8bed69f9a2f6c7086870ca05963badfd2600f94
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-10-07 09:58:53 +0700

    chore: renaming enums

---
+++ src/lib/enums/languages.ts
@@ -0,0 +1,8 @@
+export const languages = [
+  { value: 'default' },
+  { value: 'cn' },
+  { value: 'en' },
+  { value: 'id' },
+  { value: 'jp' },
+  { value: 'kr' },
+] as const
---
+++ src/lib/enums/movie-type.ts
@@ -0,0 +1,6 @@
+export const movie_type = [
+  { value: 'series' },
+  { value: 'featured_film' },
+  { value: 'short_film' },
+  { value: 'mini_series' },
+] as const
