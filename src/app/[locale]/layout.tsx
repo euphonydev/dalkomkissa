23c2687e06f4fd67f65c5815feac7a29492b17d5
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-10-11 14:56:58 +0700

    fix: locale issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   const locale = useLocale()
   let messages
   try {
-    messages = (await import(`@/locales/${locale}.json`)).default
+    messages = (await import(`@/assets/locales/${locale}.json`)).default
   } catch (error) {
     notFound()
   }
