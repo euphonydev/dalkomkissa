fc9fe0ceafd1b45cee991d83b68cc54b1778cdc5
ikusuba <73406832+ikusuba@users.noreply.github.com>
2023-10-28 13:47:38 +0700

    feat: improve data showed in discover movies
    show movie data based on current selected language

--- src/components/templates/movie-grid.tsx
+++ src/components/templates/movie-grid.tsx
@@ -1,5 +1,6 @@
 import { getMovieEntries } from '@/services/movie'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useLocale } from 'next-intl'
 import { cookies } from 'next/headers'
 import { MovieCard } from '@/components/fragments/movie-card'
 
@@ -7,7 +8,8 @@ export async function MovieGrid() {
   const supabase = createServerComponentClient({
     cookies,
   })
-  const data = await getMovieEntries(supabase, 'en', true)
+  const locale = useLocale()
+  const data = await getMovieEntries(supabase, locale, true)
   return (
     <div className="grid grid-cols-3 gap-3 sm:grid-cols-4 sm:gap-4 md:gap-5 lg:grid-cols-5 lg:gap-6 xl:grid-cols-6">
       {data.map((movie) => (
